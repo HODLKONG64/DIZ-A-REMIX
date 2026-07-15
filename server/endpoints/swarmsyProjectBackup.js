7498519d20bbd1e954e3c4741b4cbdce80ad4d65
HODL KONG 64 <hodlkong64@gmail.com>
2026-07-16 00:39:44 +0100

    Use strict project backup reader

--- server/endpoints/swarmsyProjectBackup.js
+++ server/endpoints/swarmsyProjectBackup.js
@@ -1,14 +1,12 @@
 const { userFromSession, reqBody } = require("../utils/http");
-const prisma = require("../utils/prisma");
 const { Workspace } = require("../models/workspace");
-const { SwarmsyMemoryLock } = require("../models/swarmsyMemoryLock");
-const { SwarmsyProofReview } = require("../models/swarmsyProofReview");
-const { SwarmsyIdentityIdea } = require("../models/swarmsyIdentityIdea");
-const { SwarmsyIntakeSession } = require("../models/swarmsyIntakeSession");
 const {
   buildSwarmsyProjectBackup,
   validateSwarmsyProjectBackup,
 } = require("../utils/swarmsy/projectBackup");
+const {
+  readProjectBackupSections,
+} = require("../utils/swarmsy/projectBackupReader");
 const { validatedRequest } = require("../utils/middleware/validatedRequest");
 const {
   flexUserRoleValid,
@@ -42,60 +40,6 @@ async function resolveBackupContext(request, response) {
   }
 
   return { userId, workspace };
-}
-
-async function readProjectBackupSections({ userId, workspaceId }) {
-  const [sessionRows, ideaRows, lockRows, reviewRows] = await Promise.all([
-    prisma.$queryRawUnsafe(
-      `SELECT *
-       FROM swarmsy_intake_sessions
-       WHERE workspace_id = ?
-         AND user_id = ?
-         AND is_active = true
-         AND archived_at IS NULL
-       ORDER BY version DESC
-       LIMIT 1`,
-      workspaceId,
-      userId
-    ),
-    prisma.$queryRawUnsafe(
-      `SELECT *
-       FROM swarmsy_identity_ideas
-       WHERE workspace_id = ?
-         AND user_id = ?
-         AND deleted_at IS NULL
-       ORDER BY updated_at DESC, id DESC`,
-      workspaceId,
-      userId
-    ),
-    prisma.$queryRawUnsafe(
-      `SELECT *
-       FROM swarmsy_memory_locks
-       WHERE workspace_id = ?
-         AND user_id = ?
-         AND archived_at IS NULL
-       ORDER BY is_active DESC, version DESC`,
-      workspaceId,
-      userId
-    ),
-    prisma.$queryRawUnsafe(
-      `SELECT *
-       FROM swarmsy_proof_reviews
-       WHERE workspace_id = ?
-         AND user_id = ?
-         AND archived_at IS NULL
-       ORDER BY is_active DESC, version DESC`,
-      workspaceId,
-      userId
-    ),
-  ]);
-
-  return {
-    activeSession: SwarmsyIntakeSession.publicSession(sessionRows[0] || null),
-    identityIdeas: ideaRows.map(SwarmsyIdentityIdea.publicIdea),
-    memoryLocks: lockRows.map(SwarmsyMemoryLock.publicLock),
-    proofReviews: reviewRows.map(SwarmsyProofReview.publicReview),
-  };
 }
 
 async function swarmsyProjectBackupExport(request, response) {
@@ -175,7 +119,6 @@ function registerSwarmsyProjectBackupEndpoints(app) {
 }
 
 module.exports = {
-  readProjectBackupSections,
   registerSwarmsyProjectBackupEndpoints,
   swarmsyProjectBackupExport,
   swarmsyProjectBackupValidate,
